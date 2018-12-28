045e2900f158846ee13eea1676a1f84fbc94b6f9
Lloyd Escoto <42109272+lloydescoto22@users.noreply.github.com>
2018-12-28 14:26:10 +0800

    Added Message Digest and MAC

--- Diebus-Cryptography/src/diebus/cryptography/Encryption/Encryption.java
+++ Diebus-Cryptography/src/diebus/cryptography/Encryption/Encryption.java
@@ -5,7 +5,11 @@
  */
 package diebus.cryptography.Encryption;
 
+import java.security.Key;
 import java.security.MessageDigest;
+import java.security.SecureRandom;
+import javax.crypto.KeyGenerator;
+import javax.crypto.Mac;
 
 /**
  *
@@ -13,20 +17,36 @@ import java.security.MessageDigest;
  */
 public class Encryption {
     public static StringBuffer code = new StringBuffer();
+    public static String message;
     
     public Encryption(StringBuffer code)
     {
         this.code = code;
     }
     
+    public Encryption(String message)
+    {
+        this.message = message;
+    }
     
     public static Encryption messageDigest(String msg) throws Exception
     {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         md.update(msg.getBytes());
         byte[] digest = md.digest();
-        for(int x = 0;x < digest.length; x++)
-            code.append(Integer.toHexString(0xFF & digest[x]));
+        for(int i = 0;i < digest.length; i++)
+            code.append(Integer.toHexString(0xFF & digest[i]));
         return new Encryption(code);
+    }
+    
+    public static Encryption messageAuthentication(String msg) throws Exception
+    {
+        KeyGenerator gen = KeyGenerator.getInstance("DES");
+        gen.init(new SecureRandom());
+        Key key = gen.generateKey();
+        Mac mac = Mac.getInstance("HmacSHA256");
+        mac.init(key);
+        byte[] macCode = mac.doFinal(msg.getBytes());
+        return new Encryption(new String(macCode));
     }
 }
