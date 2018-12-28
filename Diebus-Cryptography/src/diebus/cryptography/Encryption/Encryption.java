2b99aef7acda7dfb3f553c2a01132112f25330b5
Lloyd Escoto <42109272+lloydescoto22@users.noreply.github.com>
2018-12-28 15:13:02 +0800

    Added RSA

--- Diebus-Cryptography/src/diebus/cryptography/Encryption/Encryption.java
+++ Diebus-Cryptography/src/diebus/cryptography/Encryption/Encryption.java
@@ -6,8 +6,11 @@
 package diebus.cryptography.Encryption;
 
 import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.MessageDigest;
 import java.security.SecureRandom;
+import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.Mac;
 
@@ -48,5 +51,17 @@ public class Encryption {
         mac.init(key);
         byte[] macCode = mac.doFinal(msg.getBytes());
         return new Encryption(new String(macCode));
+    }
+    
+    public static Encryption rsa(String msg) throws Exception
+    {
+        KeyPairGenerator pairGen = KeyPairGenerator.getInstance("RSA");
+        pairGen.initialize(2048);
+        KeyPair pair = pairGen.genKeyPair();
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, pair.getPublic());
+        cipher.update(msg.getBytes());
+        byte[] cipherText = cipher.doFinal();
+        return new Encryption(new String(cipherText, "UTF8"));
     }
 }
