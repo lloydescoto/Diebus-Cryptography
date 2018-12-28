3826fa659a0c7bed439f5e01207ef0e19ed97087
Lloyd Escoto <42109272+lloydescoto22@users.noreply.github.com>
2018-12-28 18:17:07 +0800

    Added Ceasar Cipher

--- Diebus-Cryptography/src/diebus/cryptography/Encryption/Encryption.java
+++ Diebus-Cryptography/src/diebus/cryptography/Encryption/Encryption.java
@@ -12,6 +12,7 @@ import java.security.MessageDigest;
 import java.security.PrivateKey;
 import java.security.SecureRandom;
 import java.security.Signature;
+import java.util.Arrays;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.Mac;
@@ -37,6 +38,18 @@ public class Encryption {
     public Encryption(String message)
     {
         this.message = message;
+    }
+    
+    public static Encryption ceasarCipher(String msg, int key)
+    {
+        for(int i = 0; i < msg.length(); i++)
+        {
+            if(Character.isUpperCase(msg.charAt(i)))
+                code.append((char)(((int)msg.charAt(i) + key - 65) % 26 + 65));
+            else
+                code.append((char)(((int)msg.charAt(i) + key - 97) % 26 + 97));
+        }
+        return new Encryption(code);
     }
     
     public static Encryption sha256(String msg) throws Exception
